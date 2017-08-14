8503fe5e03fded143b67e56ec812fd12a5df8377
Annalyn Sarmiento <annalyn.c.s@gmail.com>
2017-08-13 18:41:55 -0700

    added function to add and remove styling from selected button

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
-//URL for foursquare API endpoint
-const FOURSQUARE_SEARCH_URL = "https://api.foursquare.com/v2/venues/explore"
-//URL for weather API endpoint
-const WEATHER_CALL_URL = "api.openweathermap.org/data/2.5/weather"
+$(document).ready(function () {
+    $('.category-button').click(function () {
+        $('button').removeClass("selected");
+        $(this).addClass("selected");
+    });
 
-//function to retrieve API data from Foursquare
-//include Name of place, category, address
-
-//function to retrieve API data from weather
-//include weather icon
-
-//function to iterate over API data from Foursquare and to insert new HTML
-
-//function to retrieve location from form input and use weather API to insert data into HTML
-
-
-//click on navigation button add/remove selected class
+}); // end ready
 
 //autocomplete location name in form
 function activatePlacesSearch() {
-    var input = document.getElementById('search-term');
-    var autocomplete = new google.maps.places.Autocomplete(input);
-}
+    let input = document.getElementById('search-term');
+    let autocomplete = new google.maps.places.Autocomplete(input);
+};
