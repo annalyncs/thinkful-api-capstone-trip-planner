2f4c4c27ff1200d8b83a7e9a6a0f88061e1c557f
Annalyn Sarmiento <annalyn.c.s@gmail.com>
2017-08-15 17:52:46 -0700

    added JS functionality to render API call results from FourSquare into HTML

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ $(document).ready(function () {
 
 
     const WEATHER_SEARCH_URL = "http://api.openweathermap.org/data/2.5/weather?id=524901&APPID=d86b9843fdc4941e520f985922146256"
+    const FOURSQUARE_SEARCH_URL = "https://api.foursquare.com/v2/venues/explore?&client_id=FPRD2S2RFIB4QLBNBBHNAMLYOUF2AZSZ21ZK53QYASWCRJ1Z&client_secret=FEFA44EG0YDZ0XKA1UWX5ZWLZJLE30E2GYRLGB44PKE5KZ0E&v=20170915"
 
     function enterLocation() {
         $('.search-form').submit(function (event) {
             event.preventDefault();
-            var city = $('.search-query').val();
+            let city = $('.search-query').val();
 
             $.ajax(WEATHER_SEARCH_URL, {
                 data: {
@@ -20,28 +21,71 @@ $(document).ready(function () {
                 dataType: 'jsonp',
                 type: 'GET',
                 success: function (data) {
-                    var widget = displayWeather(data);
+                    let widget = displayWeather(data);
                     $('#weather-display').html(widget);
                 }
             });
+
+            $.ajax(FOURSQUARE_SEARCH_URL, {
+                data: {
+                    near: city,
+                    venuePhotos: 1,
+                    limit: 9
+                },
+                dataType: 'jsonp',
+                type: 'GET',
+                success: function (data) {
+                    let results = data.response.groups[0].items.map(function (item, index) {
+                        return displayResults(item);
+                    });
+                    $('#foursquare-results').html(results);
+                }
+            });
+
+            function displayWeather(data) {
+                return `
+                <div class="weather-results">
+                    <h1><strong>Current Weather for ${data.name}</strong></h1>
+                    <img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png">
+                    <p><strong>Weather:</strong> ${data.weather[0].main}</p>
+                    <p><strong>Description:</strong> ${data.weather[0].description}</p>
+                    <p><strong>Temperature:</strong> ${data.main.temp} &#8457;</p>
+                    <p><strong>Min. Temperature:</strong> ${data.main.temp_min} &#8457;</p>
+                    <p><strong>Max. Temperature:</strong> ${data.main.temp_max} &#8457;</p>
+                    <p><strong>Humidity:</strong> ${data.main.humidity} &#37;</p>
+                </div>
+                `;
+            }
+
+            function displayResultsNav() {
+                return `
+                <nav role="navigation" class="navigation" id="results-navigation">
+                    <div class="options row">
+                        <button class="category-button" id="food-button">FOOD</button>
+                        <button class="category-button" id="nightlife-button">NIGHTLIFE</button>
+                        <button class="category-button" id="shops-button">SHOPS</button>
+                        <button class="category-button" id="outdoors-button">OUTDOORS</button>
+                        <button class="category-button" id="entertainment-button">ENTERTAINMENT</button>
+                    </div>
+                </nav>
+                `;
+            }
+
+            function displayResults(result) {
+                return `
+                    <div class="result col-3">
+                        <div class="result-image" style="background-image: url(https://igx.4sqi.net/img/general/width960/PF3gPzpfID61aJDeu6FWbsLDinvb4c5UbTHOiHTqX1w.jpg)" ;>
+                        </div>
+                    <div class="result-description">
+                        <h2 class="result-name">${result.venue.name}</h2>
+                        <p class="category">${result.venue.categories[0].name}</p>
+                        <p class="result-address">${result.venue.location.formattedAddress}</p>
+                    </div>
+                    </div>
+                `;
+            }
         });
-
-        function displayWeather(data) {
-            return `
-            <div class="weather-results">
-                <h1><strong>Current Weather for ${data.name}</strong></h1>
-                <img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png">
-                <p><strong>Weather:</strong> ${data.weather[0].main}</p>
-                <p><strong>Description:</strong> ${data.weather[0].description}</p>
-                <p><strong>Temperature:</strong> ${data.main.temp} &#8457;</p>
-                <p><strong>Min. Temperature:</strong> ${data.main.temp_min} &#8457;</p>
-                <p><strong>Max. Temperature:</strong> ${data.main.temp_max} &#8457;</p>
-                <p><strong>Humidity:</strong> ${data.main.humidity} &#37;</p>
-            </div>
-            `;
-        }
     }
-
 
     enterLocation();
 }); // end ready
